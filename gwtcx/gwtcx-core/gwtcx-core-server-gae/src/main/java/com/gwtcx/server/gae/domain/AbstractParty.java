ad4499c741d74b9d4c466213e1f4b895724282be
rob.ferguson@uptick.com.au <rob.ferguson@uptick.com.au@47729709-52d1-c6db-8006-95f89ab1258a>
2012-02-09 03:15:33 +0000

    Updated ContactDao, AbstractParty, ...

--- gwtcx/gwtcx-core/gwtcx-core-server-gae/src/main/java/com/gwtcx/server/gae/domain/AbstractParty.java
+++ gwtcx/gwtcx-core/gwtcx-core-server-gae/src/main/java/com/gwtcx/server/gae/domain/AbstractParty.java
@@ -14,10 +14,6 @@
 
 package com.gwtcx.server.gae.domain;
 
-// import java.util.logging.Logger;
-
-// import com.google.appengine.api.datastore.Key;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -27,14 +23,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import com.google.appengine.api.datastore.Key;
+
 /**
  * AbstractParty
  *
  * JPA defines entities called "mapped superclasses" for the situation where you
  * don't persist an actual object of a superclass type but that all subclasses of
  * that type that are entities will also persist the values for the fields of the
- * "mapped superclass". That is a "mapped superclass" has no table to store its
- * objects in a datastore.
+ * "mapped superclass".
  *
  * See: http://www.datanucleus.org/products/accessplatform/jpa/orm/inheritance.html
  *
@@ -43,23 +40,66 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public abstract class AbstractParty {
 
-  /*
+  // @DiscriminatorColumn(name = EntityTokens.PARTY_TYPE_COLUMN, discriminatorType = DiscriminatorType.STRING, length = 1)
+  // @DiscriminatorValue(value = EntityTokens.PARTY_TABLE_DISCRIMINATOR_VALUE)
+
+  enum PartyType
+  {
+     ACCOUNT("A"), CONTACT("C"), INDIVIDUAL("I"), ORGANISATION("O");
+
+     private PartyType(String abbreviation) { this.abbreviation = abbreviation; }
+     public String getAbbreviation() { return abbreviation; }
+
+     private String abbreviation;
+  }
 
   @Id
   @Column(name = EntityTokens.PARTY_KEY_COLUMN)
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   protected Key partyKey;
 
-  */
-
-  @Id
-  @Column(name = EntityTokens.PARTY_ID_COLUMN)
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  protected Long partyId;
-
   @Column(name = EntityTokens.PARTY_TYPE_COLUMN)
   @Enumerated(EnumType.STRING)
   protected String partyType;
+
+
+  public Key getPartyKey() {
+    return partyKey;
+  }
+
+  public void setPartyKey(Key partyKey) {
+    this.partyKey = partyKey;
+  }
+
+  public String getPartyType() {
+    return partyType;
+  }
+
+  public void setPartyType(String partyType) {
+    this.partyType = partyType;
+  }
+}
+
+/*
+
+   public static void main(String[] args)
+   {
+      Scanner in = new Scanner(System.in);
+      System.out.print("Enter a size: (SMALL, MEDIUM, LARGE, EXTRA_LARGE) ");
+      String input = in.next().toUpperCase();
+      Size size = Enum.valueOf(Size.class, input);
+      System.out.println("size=" + size);
+      System.out.println("abbreviation=" + size.getAbbreviation());
+      if (size == Size.EXTRA_LARGE)
+         System.out.println("Good job--you paid attention to the _.");
+   }
+
+  // import java.util.logging.Logger;
+
+  // @Id
+  // @Column(name = EntityTokens.PARTY_ID_COLUMN)
+  // @GeneratedValue(strategy = GenerationType.IDENTITY)
+  // protected Long partyId;
 
   // private static Logger logger = Logger.getLogger(AbstractParty.class.getName());
 
@@ -71,11 +111,4 @@ public abstract class AbstractParty {
     this.partyId = partyId;
   }
 
-  public String getPartyType() {
-    return partyType;
-  }
-
-  public void setPartyType(String partyType) {
-    this.partyType = partyType;
-  }
-}
+*/
