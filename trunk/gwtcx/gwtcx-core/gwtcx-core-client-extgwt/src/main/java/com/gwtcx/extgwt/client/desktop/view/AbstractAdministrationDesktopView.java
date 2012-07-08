fcaaaaed9b8c76deaa30f0f6a0468b1e935a6839
rob.ferguson@uptick.com.au <rob.ferguson@uptick.com.au@47729709-52d1-c6db-8006-95f89ab1258a>
2012-07-08 11:15:49 +0000

    Update project structure

---
+++ trunk/gwtcx/gwtcx-core/gwtcx-core-client-extgwt/src/main/java/com/gwtcx/extgwt/client/desktop/view/AbstractAdministrationDesktopView.java
@@ -0,0 +1,60 @@
+/**
+ * (C) Copyright 2010, 2011 upTick Pty Ltd
+ *
+ * Licensed under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation. You may obtain a copy of the
+ * License at: http://www.gnu.org/copyleft/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.gwtcx.extgwt.client.desktop.view;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.inject.Inject;
+import com.gwtcx.client.resources.ImageCell;
+import com.gwtcx.client.resources.PlaceholderIcons;
+import com.gwtcx.client.resources.ContextAreaIcons;
+import com.gwtplatform.mvp.client.UiHandlers;
+
+/**
+ * AbstractAdministrationView
+ */
+public abstract class AbstractAdministrationDesktopView<C extends UiHandlers> extends AbstractSettingsDesktopView<C> {
+
+  @Inject
+  public AbstractAdministrationDesktopView(String filename) {
+    super(filename);
+
+    Log.debug("AbstractAdministrationView()");
+  }
+
+  @Override
+  public SafeHtml getIcon(String data) {
+
+    if ("announcements".equalsIgnoreCase(data)) {
+      return ImageCell.makeImage(ContextAreaIcons.INSTANCE.announcements());
+    } else if ("auto_numbering".equalsIgnoreCase(data)) {
+      return ImageCell.makeImage(ContextAreaIcons.INSTANCE.autoNumbering());
+    } else if ("business_units".equalsIgnoreCase(data)) {
+        return ImageCell.makeImage(ContextAreaIcons.INSTANCE.businessUnits());
+    } else if ("system_settings".equalsIgnoreCase(data)) {
+        return ImageCell.makeImage(ContextAreaIcons.INSTANCE.systemSettings());
+    } else if ("users".equalsIgnoreCase(data)) {
+        return ImageCell.makeImage(ContextAreaIcons.INSTANCE.users());
+    } else if ("teams".equalsIgnoreCase(data)) {
+        return ImageCell.makeImage(ContextAreaIcons.INSTANCE.teams());
+    } else if ("privacy_preferences".equalsIgnoreCase(data)) {
+        return ImageCell.makeImage(ContextAreaIcons.INSTANCE.privacyPreferences());
+    } else if ("product_updates".equalsIgnoreCase(data)) {
+        return ImageCell.makeImage(ContextAreaIcons.INSTANCE.productUpdates());
+    } else {
+      return ImageCell.makeImage(PlaceholderIcons.INSTANCE.placeholder48());
+    }
+  }
+}
---
+++ trunk/gwtcx/gwtcx-core/gwtcx-core-client-extgwt/src/main/java/com/gwtcx/extgwt/client/desktop/view/AbstractDashboardsDesktopView.java
@@ -0,0 +1,55 @@
+/**
+ * (C) Copyright 2010, 2011 upTick Pty Ltd
+ *
+ * Licensed under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation. You may obtain a copy of the
+ * License at: http://www.gnu.org/copyleft/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.gwtcx.extgwt.client.desktop.view;
+
+import com.google.gwt.user.client.ui.Widget;
+import com.gwtplatform.mvp.client.UiHandlers;
+import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+
+/**
+ * AbstractDashboardsView
+ */
+public abstract class AbstractDashboardsDesktopView<C extends UiHandlers> extends ViewWithUiHandlers<C> {
+
+  protected VerticalLayoutContainer panel;
+
+  // @Inject
+  public AbstractDashboardsDesktopView() {
+    super();
+
+    panel = new VerticalLayoutContainer();
+    // panel.setStyleName(StyleTokens.contextArea);
+    panel.setWidth(AbstractPagingDesktopView.CONTEXT_AREA_WIDTH);
+    panel.setHeight(AbstractPagingDesktopView.CONTEXT_AREA_HEIGHT);
+
+    bindCustomUiHandlers();
+  }
+
+  protected void bindCustomUiHandlers() { }
+
+  @Override
+  public Widget asWidget() {
+    return panel;
+  }
+
+  public VerticalLayoutContainer getPanel() {
+    return panel;
+  }
+
+  public void setPanel(VerticalLayoutContainer panel) {
+    this.panel = panel;;
+  }
+}
