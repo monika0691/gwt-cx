bdc7ec5122356b035de60579f9a8fd7ce3bd3d01
rob.ferguson@uptick.com.au <rob.ferguson@uptick.com.au@47729709-52d1-c6db-8006-95f89ab1258a>
2012-07-08 11:15:49 +0000

    Update project structure

---
+++ gwtcx/gwtcx-core/gwtcx-core-client-extgwt/src/main/java/com/gwtcx/extgwt/client/desktop/view/AbstractReportsDesktopView.java
@@ -0,0 +1,102 @@
+/**
+ * (C) Copyright 2010, 2011 upTick Pty Ltd
+ *
+ * Licensed under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation. You may obtain a copy of the
+ * License at: http://www.gnu.org/copyleft/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.gwtcx.extgwt.client.desktop.view;
+
+import java.util.List;
+
+import com.google.inject.Inject;
+import com.gwtcx.client.resources.ToolBarIcons;
+import com.gwtcx.client.uihandlers.ReportsUiHandlers;
+import com.gwtcx.client.util.I18nUtil;
+import com.gwtcx.shared.dto.AccountsDto;
+import com.gwtplatform.mvp.client.UiHandlers;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
+import com.sencha.gxt.widget.core.client.grid.Grid;
+import com.sencha.gxt.widget.core.client.tips.ToolTipConfig;
+
+/**
+ * AbstractReportsView
+ */
+public abstract class AbstractReportsDesktopView<C extends UiHandlers> extends AbstractPagingDesktopView<C> {
+
+  // protected String recordId;
+
+  @Inject
+  public AbstractReportsDesktopView(final com.gwtcx.extgwt.client.widgets.ToolBar toolBar, final Grid<?> grid) {
+    super(toolBar, grid);
+
+    // recordId = new String("-1");
+  }
+
+  @Override
+  protected void bindCustomUiHandlers() {
+    super.bindCustomUiHandlers();
+
+    // initialise the ToolBar and register its handlers
+    initToolBar();
+
+    // initialise the StatusBar and register its handlers
+    initStatusBar();
+  }
+
+  public void setResultSet(List<AccountsDto> resultSet) {
+    // resultSet == null when there are no items in the table
+    // if (resultSet != null) {
+    //   assert getGrid() instanceof AccountsContextAreaListGrid;
+    //   ((AccountsContextAreaGrid) getGrid()).setResultSet(resultSet);
+    // }
+  }
+
+  @Override
+  protected void initToolBar() {
+
+    ToolTipConfig config = getToolBar().createToolTipConfig(I18nUtil.getConstant().newButton(), "Create a new Report");
+
+    getToolBar().addTextButton(ToolBarIcons.INSTANCE.newAccount(), I18nUtil.getConstant().newButton(), config, new SelectHandler() {
+    @Override
+      public void onSelect(SelectEvent event) {
+        if (getUiHandlers() != null) {
+          assert getUiHandlers() instanceof ReportsUiHandlers;
+          ((ReportsUiHandlers) getUiHandlers()).onNewButtonClicked();
+        }
+      }
+    });
+
+    getToolBar().addSeparator();
+
+    getToolBar().addTextButton(ToolBarIcons.INSTANCE.printPreview(), null, null);
+    getToolBar().addTextButton(ToolBarIcons.INSTANCE.export(), null, null);
+
+    getToolBar().addSeparator();
+
+    getToolBar().addTextButton(ToolBarIcons.INSTANCE.assign(), null, null);
+    getToolBar().addTextButton(ToolBarIcons.INSTANCE.delete(), null, null);
+
+    getToolBar().addSeparator();
+
+    getToolBar().addTextButton(ToolBarIcons.INSTANCE.refresh(), null, null);
+
+    getToolBar().addFill();
+  }
+
+  @Override
+  protected void initStatusBar() {
+
+  }
+}
+
+// Info.display("Click", ((TextButton) event.getSource()).getText() + " clicked");
+
---
+++ gwtcx/gwtcx-core/gwtcx-core-client-extgwt/src/main/java/com/gwtcx/extgwt/client/desktop/view/PlaceholderDesktopView.java
@@ -0,0 +1,56 @@
+/**
+ * (C) Copyright 2010, 2011 upTick Pty Ltd
+ *
+ * Licensed under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation. You may obtain a copy of the
+ * License at: http://www.gnu.org/copyleft/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.gwtcx.extgwt.client.desktop.view;
+
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.gwtplatform.mvp.client.ViewImpl;
+
+public class PlaceholderDesktopView extends ViewImpl {
+
+  private final String html = "<div>\n"
+    + "<table align=\"center\">\n"
+    + "  <tr>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+    + "  <tr>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+    + "  <tr>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+    + "  <tr>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+    + "  <tr>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+    + "  <tr>\n"
+    + "    <td style=\"font-weight:bold;\">Placeholder view</td>\n"
+    + "  </tr>\n"
+    + "  <tr>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+    + "  <tr>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+    + "  <tr>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+    + "  <tr>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+    + "  <tr>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+    + "</table>\n"
+    + "</div>\n";
+
+  private final HTMLPanel panel;
+
+  public PlaceholderDesktopView() {
+
+    panel = new HTMLPanel(getPanelHtml());
+  }
+
+  @Override
+  public Widget asWidget() {
+    return panel;
+  }
+
+  protected String getPanelHtml() {
+    return html;
+  }
+}
